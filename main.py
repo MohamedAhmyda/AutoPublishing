679ffa423adf1537589568ec2bee11566b86d301
Mohammed Ahmida <91265909+MohamedAhmyda@users.noreply.github.com>
2025-10-17 14:27:13 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
 from config import app
 import asyncio
-
+import threading, time, requests
 
 
 
 
 if __name__ == "__main__":
+
     asyncio.run(app.run())
+    
+    while True:
+        try:
+            requests.get("https://autopublishing.onrender.com")
+        except:
+            pass
+        time.sleep(600)  # every 10 minutes
